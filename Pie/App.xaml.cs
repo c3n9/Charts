90c26688ced61b15b65b88d9d244ab2a30e90205
c3n9 <arseniy11041104@gmail.com>
2023-06-18 18:48:20 +0300

    add pie live chart

---
+++ Pie/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Pie.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Pie
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static NBAEntities DB = new NBAEntities();
+    }
+}
---
+++ Pie/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pie
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public SeriesCollection seriesCollection { get; set; }
+        Random rnd = new Random();
+        public MainWindow()
+        {
+            InitializeComponent();
+            var value = new ChartValues<decimal>();
+            var salary = App.DB.PlayerInTeam.Where(x=> x.SeasonId == 2 && x.TeamId == 5).Select(p => p.Salary).ToList();
+            foreach(var cost in salary)
+            {
+                value.Add(cost);
+            }
+            seriesCollection = new SeriesCollection
+            {
+                new PieSeries
+                {
+                    Title = "Salary",
+                    Values = value,
+                    DataLabels = true,
+                    Fill = new SolidColorBrush(Color.FromArgb(100, (byte)rnd.Next(0, 255), (byte)rnd.Next(0, 255), (byte)rnd.Next(0, 255) ))
+                }
+            };
+            DataContext = this;
+        }
+    }
+}
